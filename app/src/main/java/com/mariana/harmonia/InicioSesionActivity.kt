65c33fec7cbff3f174fc39eb7940ab79f633699d
aiparraguirrecdam1 <aiparraguirrecdam1@gmail.com>
2024-01-26 08:54:35 +0100

    Fondo degradado y con notas aplicado y funcion a boton de inicio de sesión añadido

--- app/src/main/java/com/mariana/harmonia/InicioSesionActivity.kt
+++ app/src/main/java/com/mariana/harmonia/InicioSesionActivity.kt
@@ -1,11 +1,22 @@
 package com.mariana.harmonia
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class InicioSesionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_sesion2)
+    }
+
+    fun tuMetodoBoton(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun tuMetodoSalir(view: View?) {
+        finish()
     }
 }
