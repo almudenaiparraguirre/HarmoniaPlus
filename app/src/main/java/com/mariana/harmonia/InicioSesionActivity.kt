be7fac713cb6a5317707c7da9cc05b495c53bae1
aitor206 <aitorzubi2004@gmail.com>
2024-01-26 12:36:53 +0100

    funcionDegradados

--- app/src/main/java/com/mariana/harmonia/InicioSesionActivity.kt
+++ app/src/main/java/com/mariana/harmonia/InicioSesionActivity.kt
@@ -1,17 +1,43 @@
 package com.mariana.harmonia
 
 import android.content.Intent
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class InicioSesionActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.prueba_inicio_sesion_ajustar)
+        colorearTexto(R.id.titleTextView)
+        colorearTexto(R.id.registrateTextView)
+
+
     }
 
-    fun tuMetodoBoton(view: View){
+    fun colorearTexto(id: Int) {
+        val titleTextView = findViewById<TextView>(id)
+        val paint = titleTextView.paint
+        val width = paint.measureText(titleTextView.text.toString())
+
+        // Asegúrate de importar el LinearGradient correctamente
+        titleTextView.paint.shader = LinearGradient(
+            0f, 0f, width, titleTextView.textSize,
+            intArrayOf(
+                resources.getColor(R.color.rosa),
+                resources.getColor(R.color.morado)
+            ),
+            null,
+            Shader.TileMode.CLAMP
+        )
+    }
+
+
+    fun tuMetodoBoton(view: View) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
