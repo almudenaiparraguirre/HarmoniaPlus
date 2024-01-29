df191d0b2b44b6113d426a03143be927e95f1452
aitor206 <aitorzubi2004@gmail.com>
2024-01-29 12:07:24 +0100

    menuRegistro

---
+++ app/src/main/java/com/mariana/harmonia/RegistroActivity.kt
@@ -0,0 +1,11 @@
+package com.mariana.harmonia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
