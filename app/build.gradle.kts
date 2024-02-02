de753e95aa32c4ae9fafbf20d4fe955c50443ca7
azubillsor1 <azubillsor1@gmail.com>
2024-02-02 13:52:16 +0100

    ImageBrowser para perfil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,12 @@ dependencies {
     //Splash
     implementation("androidx.core:core-splashscreen:1.0.1")
 
+
+    //otors
+    implementation ("androidx.activity:activity-ktx:1.3.0")
+    implementation ("androidx.fragment:fragment-ktx:1.3.0")
+
+
    //Default
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/mariana/harmonia/activitys/PerfilUsuarioActivity.kt
+++ app/src/main/java/com/mariana/harmonia/activitys/PerfilUsuarioActivity.kt
@@ -2,13 +2,18 @@ package com.mariana.harmonia.activitys
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.ScaleAnimation
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.cardview.widget.CardView
 import com.mariana.harmonia.R
 
@@ -24,10 +29,6 @@ class PerfilUsuarioActivity : AppCompatActivity() {
             mostrarDialogImagen(imageView)
         }
 
-        val lapizEditar = findViewById<ImageView>(R.id.lapiz_editar)
-        lapizEditar.setOnClickListener {
-            mostrarOpcionesPerfil(imageView)
-        }
 
     }
 
@@ -59,25 +60,28 @@ class PerfilUsuarioActivity : AppCompatActivity() {
         dialog.show()
     }
 
-    fun mostrarOpcionesPerfil(imageView: ImageView) {
-        val builder = AlertDialog.Builder(this)
-        val inflater: LayoutInflater = layoutInflater
-        val dialogView: View = inflater.inflate(R.layout.dialog_opciones_perfil, null)
-        builder.setView(dialogView)
 
-        val dialog = builder.create()
-        val editarImagenBtn = dialogView.findViewById<View>(R.id.editarImagenBtn)
-        val cancelarBtn = dialogView.findViewById<View>(R.id.cancelarBtn)
 
-        editarImagenBtn.setOnClickListener {
-            dialog.dismiss()
+    private val seleccionarImagen =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                // Obtener la URI de la imagen seleccionada
+                val uri = result.data?.data
+                // Aquí puedes guardar la URI en tu ImageView o realizar otras operaciones
+                val imageView = findViewById<ImageView>(R.id.roundedImageView)
+                imageView.setImageURI(uri)
+            } else {
+                Toast.makeText(this, "No se seleccionó ninguna imagen", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        cancelarBtn.setOnClickListener {
-            // Cierra el diálogo sin realizar ninguna acción
-            dialog.dismiss()
-        }
+    // ...
 
-        dialog.show()
+    fun cargarImagen(view: View) {
+        Log.d("MiApp", "Intentando cargar una imagen...")
+        // Intent para abrir el navegador de archivos
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        seleccionarImagen.launch(intent)
     }
+
 }
