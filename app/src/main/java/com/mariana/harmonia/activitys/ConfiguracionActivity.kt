eee4183e0e903ffe650d748ce93f20db0af3ba58
aiparraguirrecdam1 <aiparraguirrecdam1@gmail.com>
2024-02-07 14:05:40 +0100

    Avanzando pantalla config

--- app/src/main/java/com/mariana/harmonia/activitys/ConfiguracionActivity.kt
+++ app/src/main/java/com/mariana/harmonia/activitys/ConfiguracionActivity.kt
@@ -1,14 +1,41 @@
 package com.mariana.harmonia.activitys
 
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Switch
+import androidx.core.content.ContextCompat
 import com.mariana.harmonia.R
 
 class ConfiguracionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracion)
+        val switchEfectosSonido = findViewById<Switch>(R.id.switchEfectosSonido)
+        val switchOtraOpcion = findViewById<Switch>(R.id.switchSonidos)
+
+        configurarSwitchColor(switchEfectosSonido)
+        configurarSwitchColor(switchOtraOpcion)
+    }
+
+    private fun configurarSwitchColor(switch: Switch) {
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            val thumbColor = if (isChecked) {
+                ContextCompat.getColor(this, R.color.rosa_claro)
+            } else {
+                ContextCompat.getColor(this, R.color.grisClaro)
+            }
+
+            val trackColor = if (isChecked) {
+                ContextCompat.getColor(this, R.color.rosa_claro)
+            } else {
+                ContextCompat.getColor(this, R.color.grisClaro)
+            }
+
+            switch.thumbTintList = ColorStateList.valueOf(thumbColor)
+            switch.trackTintList = ColorStateList.valueOf(trackColor)
+        }
     }
 
     fun volverModoJuego(view: View){
