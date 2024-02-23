31aa60b025d94d955834d939a8b2a2cb78cca6cf
aiparraguirrecdam1 <aiparraguirrecdam1@gmail.com>
2024-02-23 08:52:10 +0100

    Se cambia el numero de nivel en perfil_usuario_activity.xml según los niveles completados (hay que cambiarlo porque coge de una lista creada en la propia clase)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+    implementation("com.google.code.gson:gson:2.8.9")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
