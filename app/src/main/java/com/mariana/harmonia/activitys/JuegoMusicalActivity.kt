2ad15714eca9080ad6800d1763bc2aae1daee943
azubillagasdam1 <azubillagasdam1@gmail.com>
2024-02-29 19:05:19 +0100

    Añadidas nuevas notas pero no reconoce el audio

--- app/src/main/java/com/mariana/harmonia/activitys/JuegoMusicalActivity.kt
+++ app/src/main/java/com/mariana/harmonia/activitys/JuegoMusicalActivity.kt
@@ -387,7 +387,11 @@ class JuegoMusicalActivity : AppCompatActivity() {
         if (aciertos != null && aciertos!! < notasArray.size) {
             // Si la nota es la indicada entra
             if (notasArray[aciertos!!]?.substring(1) == nombreNota) {
+                var nota = notasArray[aciertos!!]
+                playSound(nota!!)
                 aciertos = aciertos?.plus(1)
+
+
                 // Comprobar si aciertos sigue siendo menor que la longitud de notasArray
                 if (aciertos!! < notasArray.size) {
                     cambiarImagen(notasArray[aciertos!!].toString())
@@ -543,8 +547,7 @@ class JuegoMusicalActivity : AppCompatActivity() {
 
 
     private fun playSound(soundFile: String) {
-        val mediaPlayer =
-            MediaPlayer.create(this, resources.getIdentifier(soundFile, "raw", packageName))
+        val mediaPlayer = MediaPlayer.create(this, resources.getIdentifier("sound_$soundFile", "raw", packageName))
         val volume = 1f
         mediaPlayer.setVolume(volume, volume)
         mediaPlayer.start()
@@ -590,7 +593,7 @@ class JuegoMusicalActivity : AppCompatActivity() {
     private fun clickDo() {
         Log.d("pruebasActivity", "Se ha hecho clic en la nota Do")
 
-        playSound("c4")
+
         actualizarFondoBlancas(R.id.notaDo, R.drawable.svg_tecla_do_clicada, this)
         cambiarImagen("nota_6d")
         comprobarJugada("c")
@@ -599,7 +602,7 @@ class JuegoMusicalActivity : AppCompatActivity() {
     fun clickRe_b() {
         Log.d("pruebasActivity", "Se ha hecho clic en el método Reb")
 
-        playSound("db4")
+
         actualizarFondoNegras(R.id.fondoReB, R.drawable.style_buttond_degradado_suave)
         comprobarJugada("db")
     }
@@ -607,7 +610,7 @@ class JuegoMusicalActivity : AppCompatActivity() {
     private fun clickRe() {
         Log.d("pruebasActivity", "Se ha hecho clic en la nota Re")
 
-        playSound("d4")
+
         actualizarFondoBlancas(R.id.notaRe, R.drawable.svg_tecla_re_clicada, this)
         comprobarJugada("d")
     }
@@ -616,16 +619,16 @@ class JuegoMusicalActivity : AppCompatActivity() {
     fun clickMi_b() {
         Log.d("pruebasActivity", "Se ha hecho clic en el método Mib")
 
-        playSound("eb4")
+
         actualizarFondoNegras(R.id.fondoMiB, R.drawable.style_buttond_degradado_suave)
-        comprobarJugada("mb")
+        comprobarJugada("eb")
     }
 
 
     private fun clickMi() {
         Log.d("pruebasActivity", "Se ha hecho clic en la nota Mi")
 
-        playSound("e4")
+
         actualizarFondoBlancas(R.id.notaMi, R.drawable.svg_tecla_mi_clicada, this)
         comprobarJugada("e")
     }
@@ -633,7 +636,7 @@ class JuegoMusicalActivity : AppCompatActivity() {
     private fun clickFa() {
         Log.d("pruebasActivity", "Se ha hecho clic en la nota Fa")
 
-        playSound("f4")
+
         actualizarFondoBlancas(R.id.notaFa, R.drawable.svg_tecla_fa_clicada, this)
         comprobarJugada("f")
     }
@@ -641,7 +644,7 @@ class JuegoMusicalActivity : AppCompatActivity() {
     fun clickSol_b() {
         Log.d("pruebasActivity", "Se ha hecho clic en el método Solb")
 
-        playSound("gb4")
+
         actualizarFondoNegras(R.id.fondoSolB, R.drawable.style_buttond_degradado_suave)
         comprobarJugada("gb")
     }
@@ -649,7 +652,7 @@ class JuegoMusicalActivity : AppCompatActivity() {
     private fun clickSol() {
         Log.d("pruebasActivity", "Se ha hecho clic en la nota Sol")
 
-        playSound("g4")
+
         actualizarFondoBlancas(R.id.notaSol, R.drawable.svg_tecla_sol_clicada, this)
         comprobarJugada("g")
     }
@@ -658,7 +661,7 @@ class JuegoMusicalActivity : AppCompatActivity() {
     fun clickLa_b() {
         Log.d("pruebasActivity", "Se ha hecho clic en el método Lab")
 
-        playSound("ab4")
+
         actualizarFondoNegras(R.id.fondoLaB, R.drawable.style_buttond_degradado_suave)
         comprobarJugada("ab")
 
@@ -667,7 +670,7 @@ class JuegoMusicalActivity : AppCompatActivity() {
     private fun clickLa() {
         Log.d("pruebasActivity", "Se ha hecho clic en la nota La")
 
-        playSound("a4")
+
         actualizarFondoBlancas(R.id.notaLa, R.drawable.svg_tecla_la_clicada, this)
         comprobarJugada("a")
 
@@ -676,7 +679,7 @@ class JuegoMusicalActivity : AppCompatActivity() {
     fun clickSi_b() {
         Log.d("pruebasActivity", "Se ha hecho clic en el método Sib")
 
-        playSound("bb4")
+
         actualizarFondoNegras(R.id.fondoSiB, R.drawable.style_buttond_degradado_suave)
         comprobarJugada("bb")
     }
@@ -684,7 +687,7 @@ class JuegoMusicalActivity : AppCompatActivity() {
     private fun clickSi() {
         Log.d("pruebasActivity", "Se ha hecho clic en la nota Si")
 
-        playSound("b4")
+
         actualizarFondoBlancas(R.id.notaSi, R.drawable.svg_tecla_si_clicada, this)
         comprobarJugada("b")
     }
