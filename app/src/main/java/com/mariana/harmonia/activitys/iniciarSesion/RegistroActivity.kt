666c90a3b9122cac7192d7f54fbdb5ec5511d0dd
azubillsor1 <azubillsor1@gmail.com>
2024-03-05 09:08:56 +0100

    Separado encriptador a clase individual

--- app/src/main/java/com/mariana/harmonia/activitys/iniciarSesion/RegistroActivity.kt
+++ app/src/main/java/com/mariana/harmonia/activitys/iniciarSesion/RegistroActivity.kt
@@ -20,6 +20,7 @@ import com.mariana.harmonia.interfaces.PlantillaActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
 import com.mariana.harmonia.models.entity.User
+import com.mariana.harmonia.utils.HashUtils
 import java.security.MessageDigest
 import java.time.LocalDate
 
@@ -114,29 +115,7 @@ class RegistroActivity : AppCompatActivity(), PlantillaActivity {
         return result.toString()
     }
 
-    object HashUtils {
-        fun sha512(input: String) = hashString("SHA-512", input)
 
-        fun sha256(input: String) = hashString("SHA-256", input)
-
-        fun sha1(input: String) = hashString("SHA-1", input)
-
-        private fun hashString(type: String, input: String): String {
-            val HEX_CHARS = "0123456789ABCDEF"
-            val bytes = MessageDigest
-                .getInstance(type)
-                .digest(input.toByteArray())
-            val result = StringBuilder(bytes.size * 2)
-
-            bytes.forEach {
-                val i = it.toInt()
-                result.append(HEX_CHARS[i shr 4 and 0x0f])
-                result.append(HEX_CHARS[i and 0x0f])
-            }
-
-            return result.toString()
-        }
-    }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun registrarUsuarioEnFirebase(email: String, contraseña: String, nombre: String) {
--- app/src/main/java/com/mariana/harmonia/utils/Utils.kt
+++ app/src/main/java/com/mariana/harmonia/utils/Utils.kt
@@ -22,6 +22,7 @@ import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStream
+import java.security.MessageDigest
 import java.util.Locale
 
 class Utils {
@@ -300,6 +301,22 @@ class Utils {
             return false
         }
 
+         fun hashString(type: String, input: String): String {
+            val HEX_CHARS = "0123456789ABCDEF"
+            val bytes = MessageDigest
+                .getInstance(type)
+                .digest(input.toByteArray())
+            val result = StringBuilder(bytes.size * 2)
+
+            bytes.forEach {
+                val i = it.toInt()
+                result.append(HEX_CHARS[i shr 4 and 0x0f])
+                result.append(HEX_CHARS[i and 0x0f])
+            }
+
+            return result.toString()
+
+    }
 
     }
 }
