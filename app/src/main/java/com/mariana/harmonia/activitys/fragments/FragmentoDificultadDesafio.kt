bfa19edf700b56b3325616d2fd83a698bf06d089
aiparraguirrecdam1 <aiparraguirrecdam1@gmail.com>
2024-03-11 12:50:02 +0100

    Probando a añadir comentarios Dokka para DEIN y eliminada foto de Luis

--- app/src/main/java/com/mariana/harmonia/activitys/fragments/FragmentoDificultadDesafio.kt
+++ app/src/main/java/com/mariana/harmonia/activitys/fragments/FragmentoDificultadDesafio.kt
@@ -2,7 +2,6 @@ package com.mariana.harmonia.activitys.fragments
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,45 +10,45 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.mariana.harmonia.R
 import com.mariana.harmonia.activitys.JuegoMusicalActivity
-import com.mariana.harmonia.utils.Utils
 
+/**
+ * FragmentoDificultadDesafio es un Fragment que permite al usuario seleccionar la dificultad
+ * para un desafío musical. Contiene botones para las dificultades "Fácil", "Medio" y "Difícil".
+ */
 class FragmentoDificultadDesafio : Fragment() {
 
-
-
+    // Declaración de variables para los botones y TextView
     private lateinit var botonFacil: androidx.appcompat.widget.AppCompatButton
     private lateinit var botonMedio: androidx.appcompat.widget.AppCompatButton
     private lateinit var botonFificil: androidx.appcompat.widget.AppCompatButton
     private lateinit var textViewVolver: TextView
 
-
+    /**
+     * Método llamado para crear y devolver la jerarquía de vistas asociada con el fragmento.
+     * @param inflater El objeto LayoutInflater que puede inflar vistas en el contexto actual.
+     * @param container Si no es nulo, este es el grupo de vistas al que se adjunta el fragmento.
+     * @param savedInstanceState Si no es nulo, este fragmento está siendo reconstituido a partir de un estado guardado previamente.
+     * @return Devuelve la Vista principal del fragmento.
+     */
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-
     ): View? {
-
-        // Infla el diseño del fragmento que contiene los botones
         val view = inflater.inflate(R.layout.fragment_dificultad_desafio, container, false)
 
-
-        // Encuentra los botones en el diseño
         botonFacil = view.findViewById(R.id.botonFacil)
         botonMedio = view.findViewById(R.id.botonMedio)
         botonFificil = view.findViewById(R.id.botonFificil)
         textViewVolver = view.findViewById(R.id.textViewVolver)
-        //Utils.degradadoTexto(requireActivity(),R.id.tituloDificultad,R.color.rosa,R.color.morado)
 
-        // Configura el comportamiento de los botones
         botonFacil.setOnClickListener {
             val intent = Intent(requireActivity(), JuegoMusicalActivity::class.java)
             intent.putExtra("desafio", true)
             intent.putExtra("dificultad", 0)
             requireActivity().startActivity(intent)
 
-            // Cierra el fragmento actual
             requireActivity().supportFragmentManager.popBackStack()
         }
 
@@ -59,7 +58,6 @@ class FragmentoDificultadDesafio : Fragment() {
             intent.putExtra("dificultad", 1)
             requireActivity().startActivity(intent)
 
-            // Cierra el fragmento actual
             requireActivity().supportFragmentManager.popBackStack()
         }
 
@@ -69,20 +67,13 @@ class FragmentoDificultadDesafio : Fragment() {
             intent.putExtra("dificultad", 2)
             requireActivity().startActivity(intent)
 
-
-            // Cierra el fragmento actual
             requireActivity().supportFragmentManager.popBackStack()
         }
-
 
         textViewVolver.setOnClickListener {
             requireActivity().supportFragmentManager.popBackStack()
-
         }
-
 
         return view
     }
-
-
 }
