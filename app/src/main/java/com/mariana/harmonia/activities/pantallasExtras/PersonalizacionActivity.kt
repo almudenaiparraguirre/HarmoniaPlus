68b1d1ff66769d14a68077b28179c6877304a14c
azubillagasdam1 <azubillagasdam1@gmail.com>
2024-06-13 13:55:29 +0200

    Comenzado el diseño de ConfiguracionActivity

--- app/src/main/java/com/mariana/harmonia/activities/pantallasExtras/PersonalizacionActivity.kt
+++ app/src/main/java/com/mariana/harmonia/activities/pantallasExtras/PersonalizacionActivity.kt
@@ -17,7 +17,7 @@ class PersonalizacionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_personalizacion)
+        setContentView(R.layout.personalizacion_activity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
